49171e3d4135a9d2b4e02360a0aa203a81f6f797
Adriswara <chevellerz454@yahoo.com>
2023-07-06 15:20:14 +0700

    changs fieldname and DB name
    Refactored

---
+++ UASPbo/src/DBConnection.java
@@ -0,0 +1,150 @@
+import java.sql.*;
+import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class DBConnection { // class DBConnection. bertanggung jawab untuk mengatur koneksi ke database.
+    public String url;  // variabel String digunakan untuk menyimpan url yang diperlukan untuk koneksi ke database,
+    public String username; // variabel String digunakan untuk menyimpan username yang diperlukan untuk koneksi ke database,
+    public String password; // variabel String digunakan untuk menyimpan password yang diperlukan untuk koneksi ke database,
+    
+    static Connection conn; // conn adalah variabel Connection yang akan digunakan untuk menyimpan objek koneksi ke database.
+    static Statement stmt; // stmt adalah variabel Statement yang akan digunakan untuk mengeksekusi pernyataan SQL pada database.
+    static ResultSet rs; // rs adalah variabel ResultSet yang akan digunakan untuk menyimpan hasil query dari database.
+    
+    static InputStreamReader InputStreamReader = new InputStreamReader(System.in); // digunakan untuk membaca karakter dari System.in.
+    static BufferedReader input = new BufferedReader(InputStreamReader); // digunakan untuk membaca baris input dari InputStreamReader.
+
+    public DBConnection(String url, String username, String password) { // konstruktor
+        this.url = url; // inisialisasi var url dengan isi yang ada dalam parameter url
+        this.username = username; // inisialisasi var username dengan isi yang ada dalam parameter username
+        this.password = password; // inisialisasi var password dengan isi yang ada dalam parameter password
+                
+    }
+    
+    
+    public void displayData(){ // fungsi untuk membaca/mendisplay data yang ada di database
+        try{
+            Class.forName("com.mysql.jdbc.Driver"); // digunakan untuk memuat kelas driver JDBC MySQL.
+            Connection conn = DriverManager.getConnection(url, username, password);
+            // digunakan untuk membuat koneksi ke database dengan menggunakan nilai dari variabel url, username, dan password.
+            Statement stmt = conn.createStatement();
+            // digunakan untuk membuat objek Statement yang akan digunakan untuk mengeksekusi pernyataan SQL.
+            String query = "SELECT * FROM todo";
+            // var query bertipe data string untuk menyimpan query SQL yang akan dieksekusi.
+            ResultSet rs = stmt.executeQuery(query);
+            // digunakan untuk menjalankan query SQL dan menyimpan hasilnya dalam objek ResultSet rs.
+            
+            while (rs.next()) { // untuk menglooping setiap baris hasil query. 
+                int id = rs.getInt("id"); // var id di inisialisasi dengan nilai dari rs.getInt("id")
+                String todo = rs.getString("todo"); // var nama_barang di inisialisasi dengan nilai dari rs.getString("nama_barang")
+                String kategori = rs.getString("kategori"); // var harga di inisialisasi dengan nilai dari rs.getInt("harga")
+                String tanggalselesai = rs.getString("tanggalselesai"); // var ukuran di inisialisasi dengan nilai dari rs.getString("ukuran")
+                String status = rs.getString("status"); // var ukuran di inisialisasi dengan nilai dari rs.getString("ukuran")
+
+                
+                // sebuah label yang menampilkan data dari database
+                System.out.println("Data Barang \n==============\n" // sebuah label
+                                    + "id = " + id + "\n"  // menampilkan data id
+                                    + "todo  = " + todo + "\n" // menampilkan data nama barang
+                                    + "kategori = " + kategori + "\n"  // menampilkan data harga
+                                    + "tanggalselesai = " + tanggalselesai +"\n"   // // menampilkan data ukuran
+                                    + "status = " + status);  // // menampilkan data ukuran
+                
+               
+        } 
+             rs.close(); // menutup aliran/stream objek rs
+            stmt.close(); // menutup aliran/stream objek stmt
+            conn.close(); // menutup aliran/stream objek conn
+        } catch(ClassNotFoundException e){ // menangkap dan menangani exception yang mungkin terjadi selama eksekusi di try
+             System.out.println("Failed to load JDBC driver"); // menampilkan pesan errornya
+            e.printStackTrace(); // digunakan untuk mencetak trace exception ke output standar.
+        } catch (SQLException e) { // menangkap dan menangani exception yang mungkin terjadi selama eksekusi di try
+            System.out.println("Database connection error");
+            e.printStackTrace(); // digunakan untuk mencetak trace exception ke output standar.
+        } catch(NullPointerException e){ // menangkap dan menangani exception yang mungkin terjadi selama eksekusi di try
+            e.printStackTrace(); // digunakan untuk mencetak trace exception ke output standar.
+        }
+            
+        
+    }
+    public void insertData(){ // fungsi untuk menambahkan data ke dalam database
+        try {
+            conn = DriverManager.getConnection(url, username, password); 
+            // digunakan untuk membuat koneksi ke database dengan menggunakan nilai dari variabel url, username, dan password
+            stmt = conn.createStatement();
+            // digunakan untuk membuat objek Statement yang akan digunakan untuk mengeksekusi pernyataan SQL.
+            
+        // ambil input dari user
+        System.out.print("Nama Barang: "); // sebuah label
+        String nb = input.readLine().trim(); // membaca inputan dari keyboard
+        System.out.print("Ukuran: "); // sebuah label
+        String ukuran = input.readLine().trim(); // membaca inputan dari keyboard
+        System.out.print("Harga: "); // sebuah label
+        String harga = input.readLine().trim(); // membaca inputan dari keyboard
+        
+        // query simpan
+        String sql = "INSERT INTO dbtokoseragam (nama_barang, ukuran, harga) VALUES('"+nb+"', '"+ukuran+"','"+harga+"')";
+        // var sql bertipe data string untuk menyimpan query SQL yang akan dieksekusi.
+        sql = String.format(sql, nb, ukuran, harga);
+        
+        // simpan barang
+        stmt.execute(sql); // digunakan untuk menjalankan pernyataan SQL yang menyisipkan data baru ke dalam tabel.
+        
+    } catch (Exception e) { // menangkap dan menangani exception yang mungkin terjadi selama eksekusi di try
+        e.printStackTrace(); // digunakan untuk mencetak trace exception ke output standar.
+    }
+    }
+    public void updateData(){ // fungsi untuk mengubah data dalam database
+        try {  
+            conn = DriverManager.getConnection(url, username, password); 
+            // digunakan untuk membuat koneksi ke database dengan menggunakan nilai dari variabel url, username, dan password
+            stmt = conn.createStatement();
+            // digunakan untuk membuat objek Statement yang akan digunakan untuk mengeksekusi pernyataan SQL.
+            
+        // ambil input dari user
+        System.out.print("pilih ID yang mau diedit: "); // sebuah label
+        int idBarang = Integer.parseInt(input.readLine()); // membaca inputan dari keyboard
+        System.out.print("Nama: "); // sebuah label
+        String nb = input.readLine().trim(); // membaca inputan dari keyboard
+        System.out.print("Ukuran: "); // sebuah label
+        String ukuran = input.readLine().trim(); // membaca inputan dari keyboard
+        System.out.print("Harga: "); // sebuah label
+        String harga = input.readLine().trim(); // membaca inputan dari keyboard
+
+        // query update
+        String sql = "UPDATE dbtokoseragam SET nama_barang='"+nb+"', ukuran='"+ukuran+"', harga='"+harga+"' WHERE id='"+idBarang+"'";
+        // var sql bertipe data string untuk menyimpan query SQL yang akan dieksekusi.
+        
+        
+        stmt.execute(sql); // digunakan untuk menjalankan pernyataan SQL yang menyisipkan data baru ke dalam tabel.
+        
+    } catch (Exception e) { // menangkap dan menangani exception yang mungkin terjadi selama eksekusi di try
+        e.printStackTrace(); // digunakan untuk mencetak trace exception ke output standar.
+    }
+    }
+    public void deleteData(){ // fungsi untuk menghapus data dalam database
+        try {
+        conn = DriverManager.getConnection(url, username, password);
+        // digunakan untuk membuat koneksi ke database dengan menggunakan nilai dari variabel url, username, dan password
+        stmt = conn.createStatement();
+        // digunakan untuk membuat objek Statement yang akan digunakan untuk mengeksekusi pernyataan SQL.
+        
+        // ambil input dari user
+        System.out.print("pilih ID yang mau dihapus: "); // sebuah label
+        int idBarang = Integer.parseInt(input.readLine()); // membaca inputan dari keyboard 
+        
+        // buat query hapus
+        String sql = String.format("DELETE FROM dbtokoseragam WHERE id=%d", idBarang);
+        // var sql bertipe data string untuk menyimpan query SQL yang akan dieksekusi.
+        
+        // hapus data
+        stmt.execute(sql); // digunakan untuk menjalankan pernyataan SQL yang menyisipkan data baru ke dalam tabel.
+        
+        System.out.println("Data telah terhapus..."); // sebuah label
+    } catch (Exception e) { // menangkap dan menangani exception yang mungkin terjadi selama eksekusi di try
+        e.printStackTrace(); // digunakan untuk mencetak trace exception ke output standar.
+    }
+    }
+}
--- UASPbo/src/Tester.java
+++ UASPbo/src/Tester.java
@@ -1,20 +1,74 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.sql.*; // mengimport kelas-kelas yang terkait dengan JDBC untuk berinteraksi dengan database. 
+import java.io.InputStreamReader; //InputStreamReader digunakan untuk mengkonversi byte input menjadi karakter.
+import java.io.BufferedReader; // digunakan untuk membaca teks dengan lebih efisien, 
+// karena ia melakukan pembacaan sekelompok karakter (buffer) sekaligus daripada karakter per karakter.
+import java.io.IOException; //  I/O exceptions digunakan untuk menangani situasi yang tidak terduga atau 
+// error yang terjadi saat membaca atau menulis data ke sumber daya input/output, seperti berkas, socket, atau input stream.
 
-/**
- *
- * @author Noise Culture
- */
 public class Tester {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        static InputStreamReader InputStreamReader = new InputStreamReader(System.in); // mendeklarasikan objek, InputStreamReader 
+        // untuk membaca input dari pengguna melalui konsol.
+        static BufferedReader input = new BufferedReader(InputStreamReader); // mendeklarasikan objek, BufferedReader, 
+        // untuk membaca input dari pengguna melalui konsol.
+
+    public static void main(String[] args) throws IOException{ // metode utama yang akan dieksekusi saat program Java dijalankan. 
+        // Ia juga menunjukkan bahwa ada kemungkinan terjadi IOException
+        String url = "jdbc:mysql://localhost/uaspbo"; // inisialisasi var url dengan isi url koneksi java dengan mysql database
+        String username = "root"; // inisialisasi var username dengan isi username mysql
+        String password = ""; // inisialisasi var password dengan isi password mysql
+        
+        
+        DBConnection koneksiDB = new DBConnection(url, username, password);
+        // membuat objek DBConnection dengan menggunakan informasi koneksi ke database.
+        boolean loop = true; // var loop diinisialisasi sebagai true untuk memulai loop program.
+        do { // yang menampilkan menu program kepada pengguna dan membaca pilihan pengguna dari konsol menggunakan objek 
+        System.out.println("Menu Program"); // sebuah label
+        System.out.println("============="); // sebuah label
+        System.out.println("1. Insert activity"); // sebuah label
+        System.out.println("2. Show data todo list"); // sebuah label
+        System.out.println("3. Edit activity"); // sebuah label
+        System.out.println("4. Delete activity"); // sebuah label
+        System.out.println("5. Exit"); // sebuah label
+        System.out.print("masukkan pilihan : "); // sebuah label
+        
+        
+        
+            try {
+            int pilih = Integer.parseInt(input.readLine()); //  Membaca input dari pengguna dan mengkonversinya menjadi bilangan bulat. 
+            // Input ini akan menentukan pilihan menu yang akan dieksekusi.
+            
+            switch(pilih){ // digunakan untuk memilih tindakan berdasarkan nilai pilih
+                case 1: {
+                    koneksiDB.insertData(); // : Jika pilih sama dengan 1, maka memanggil metode insertData() dari objek koneksiDB
+                    break;
+                }
+                case 2:{
+                    koneksiDB.displayData(); // : Jika pilih sama dengan 2, maka memanggil metode displayData() dari objek koneksiDB
+                    break;
+                }   
+                case 3: {
+                    koneksiDB.updateData(); // : Jika pilih sama dengan 3, maka memanggil metode updateData() dari objek koneksiDB
+                    break;
+                }
+                case 4: {
+                    koneksiDB.deleteData(); // : Jika pilih sama dengan 4, maka memanggil metode deleteData() dari objek koneksiDB
+                    break;
+                }
+                case 5: { // : Jika pilih sama dengan 5, maka keluar dari program/loop
+                    loop = false;
+                    System.exit(0);
+                    break;
+                }
+                default:
+                    System.out.println("Pilihan yang anda masukkan salah woy!"); // jika selain dari pilihan tampilkan pesan ini
+            }
+            
+        } catch (IOException | NumberFormatException e) {
+            
+        }
+    } while(loop);
+        }
+        
     
 }
